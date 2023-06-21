40a0de9f306b41425ce8a593af8600fc25d84e89
“helal28d” <helal.dahi1992@gmail.com>
2023-06-21 07:32:32 -0500

    /

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "images");
+    cb(null, "/images");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
